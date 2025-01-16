a0df805a5d7afa3db9d788a49185c9869cca8fa9
rekky1aws <killikomfr@gmail.com>
2025-01-16 20:19:19 +0100

    Init

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<script type="text/javascript"></script>
+
+<template>
+	<header class="bg-green-400 p-5">
+		<img src="">
+		<h1 class="text-3xl">Donatien HOCHART</h1>
+		<h2 class="text-xl">Développeur Web Fullstack</h2>
+	</header>
+</template>
+
+<style type="text/css"></style>
