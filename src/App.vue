9384e6c45739aa519db85b6ae78a4272d1ab7ef3
rekky1aws <killikomfr@gmail.com>
2025-01-21 20:19:19 +0100

    Main element vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from './components/Header.vue'
+import Main from './components/Main.vue'
 </script>
 
 <template>
   <Header></Header>
+  <Main></Main>
 </template>
 
 <style scoped>
---
+++ src/components/Main.vue
@@ -0,0 +1,13 @@
+<script>
+import Presentation from './sections/Presentation.vue'
+</script>
+
+<template>
+	<main>
+		
+	</main>
+</template>
+
+<style>
+	
+</style>
