9de29de65dd4bbd483f36cf6d9fd6065fec38c4d
rekky1aws <killikomfr@gmail.com>
2025-01-25 02:16:18 +0100

    Setting the grid up in Skills.vue

--- src/components/sections/Skills.vue
+++ src/components/sections/Skills.vue
@@ -3,7 +3,19 @@
 </script>
 
 <template>
-	
+	<section id="skills">
+		<h3 class="text-xl underline">Skills</h3>
+		<div class="grid grid-rows-4 grid-cols-3 gap-4">
+			<a href=""><img src=""></a>
+			<a href=""><img src=""></a>
+			<a href=""><img src=""></a>
+			<a href=""><img src=""></a>
+			<a href=""><img src=""></a>
+			<a href=""><img src=""></a>
+			<a href=""><img src=""></a>
+			<a href=""><img src=""></a>
+		</div>
+	</section>
 </template>
 
 <style>
