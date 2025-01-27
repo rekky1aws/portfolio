c8f045eb57f249aeb246e844dc2fd78f5f00f681
rekky1aws <killikomfr@gmail.com>
2025-01-27 17:36:18 +0100

    Used new colors in gradient

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,12 @@
 <script type="text/javascript"></script>
 
 <template>
-	<header class="bg-green-400 p-5">
-		<img src="">
-		<h1 class="text-3xl">Donatien HOCHART</h1>
-		<h2 class="text-xl">Développeur Web Fullstack</h2>
+	<header class="bg-gradient-to-tr from-secondarycyan to-maingreen p-5 flex items-center justify-between sticky top-0">
+		<div>
+			<h1 class="text-3xl">Donatien HOCHART</h1>
+			<h2 class="text-xl">Développeur Web Fullstack</h2>
+		</div>
+		<img src="/public/pp.jpg" class="size-16 rounded-full ease-linear duration-200 right-5 top-5 fixed hover:drop-shadow hover:size-60">
 	</header>
 </template>
 
