53b201b6ee7edddf3ffaad544a113ea317322552
rekky1aws <killikomfr@gmail.com>
2025-01-29 20:19:19 +0100

    Changed Header for a better look.
    Removed Header gradient background and used a flat gray one to stick
    better with the upcoming new style.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from './components/Header.vue'
 import Main from './components/Main.vue'
+import Banner from './components/Banner.vue'
 </script>
 
 <template>
+  <Banner></Banner>
   <Header></Header>
   <Main></Main>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,19 @@
 <script type="text/javascript"></script>
 
 <template>
-	<header class="bg-gradient-to-tr from-secondarycyan to-maingreen p-5 flex items-center justify-between sticky top-0">
-		<div>
-			<h1 class="text-3xl">Donatien HOCHART</h1>
-			<h2 class="text-xl">Développeur Web Fullstack</h2>
+	<header class="bg-gray-200 drop-shadow p-5 flex flex-reverse items-center justify-between sticky top-0">
+		<div class="flex gap-5">
+			<img src="/pp.jpg" class="size-16 rounded-full">
+			<div>
+				<h1 class="text-3xl">Donatien HOCHART</h1>
+				<h2 class="text-xl">Développeur Web Fullstack</h2>
+			</div>
 		</div>
-		<img src="/public/pp.jpg" class="size-16 rounded-full ease-linear duration-200 right-5 top-5 fixed hover:drop-shadow hover:size-60">
+
+
+		<nav>
+				
+		</nav>
 	</header>
 </template>
 
