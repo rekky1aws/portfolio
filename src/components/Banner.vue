053ef07d647036a0245bbd52635fad09152a6273
rekky1aws <killikomfr@gmail.com>
2025-01-30 16:46:46 +0100

    Fixed banner
    Made banner image taller and fixed height

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -2,16 +2,14 @@
 </script>
 
 <template>
-  <section class="bg-[url(/banner.png)] min-h-full max-h-screen w-screen p-16">
-    <div class="h-screen w-1/3 flex flex-col gap-6">
+  <section class="bg-[url(/banner.png)] bg-center h-96 w-full p-16 h-full flex flex-col gap-4">
+    <div class="flex flex-col gap-1 w-1/3">
       <h3 class="text-5xl font-bold">Hi, I'm Donatien !</h3>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </div>    
+      <h4 class="text-4xl" id="work">Full-Stack Developper</h4>
+    </div>  
+    <p class="w-1/3">
+      I am a Full-Stack Developer. Currently starting my freelance business and open to work.
+    </p>   
   </section>
 </template>
 
