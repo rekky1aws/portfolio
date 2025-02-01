0b432925505403e73c73365f9a0fea94188669db
rekky1aws <killikomfr@gmail.com>
2025-02-01 17:13:05 +0100

    Banner refactor
    Moved class to the div encapsulating the p tags instead of duping the
    code in both of them.

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -7,12 +7,12 @@
       <h3 class="sm:text-3xl md:text-4xl lg:text-5xl font-bold">Hi, I'm Donatien !</h3>
       <h4 class="sm:text-2xl md:text-3xl lg:text-4xl" id="work">Full-Stack Developper</h4>
     </div>
-    <div>  
-      <p class="text-xs italic sm:text-sm md:text-base w-1/3">
+    <div class="text-xs italic sm:text-sm md:text-base w-1/3">  
+      <p>
         I am a Full-Stack Developer. Currently starting my freelance business. 
       </p>   
-      <p class="hidden xl:inline italic">
-        I'm eager to start working with you if you need me.
+      <p class="hidden xl:block">
+        If you need someone like me, don't hesitate because it'll be a pleasure to start working with you.
       </p>
     </div>
   </section>
