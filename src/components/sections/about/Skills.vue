23445d849366bbb7d32bc4803cc8fb4d6c27608f
rekky1aws <killikomfr@gmail.com>
2025-02-04 20:19:19 +0100

    Pruned useless classes in Skills.vue

--- src/components/sections/about/Skills.vue
+++ src/components/sections/about/Skills.vue
@@ -4,7 +4,7 @@
 
 <template>
 	<article id="skills">
-		<h6 class="text-xl underline">Skills</h6>
+		<h6>Skills</h6>
 		<div class="grid grid-rows-4 grid-cols-3 gap-4">
 			<a href=""><img src=""></a>
 			<a href=""><img src=""></a>
