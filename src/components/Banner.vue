331ff039c5f17bb8d2096ec9efda7852ddfd3521
rekky1aws <killikomfr@gmail.com>
2025-02-05 15:43:01 +0100

    Better Banner
    Improved banner and its responsive behavior by rendering the grandient
    outside of the image.

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -2,19 +2,22 @@
 </script>
 
 <template>
-  <section class="bg-[url(/banner.png)] bg-no-repeat bg-center h-96 w-full p-16 h-full flex flex-col gap-4">
-    <div class="flex flex-col gap-1 w-1/3">
-      <h3 class="sm:text-3xl md:text-4xl lg:text-5xl font-bold">Hi, I'm Donatien !</h3>
-      <h4 class="sm:text-2xl md:text-3xl lg:text-4xl" id="work">Full-Stack Developper</h4>
+  <section class="bg-gradient-to-tr from-secondarycyan to-maingreen bg-no-repeat w-full flex flex-row justify-between items-center m-0 p-0">
+    <div class="h-96 p-16 h-full flex flex-col gap-4">
+      <div class="flex flex-col gap-1">
+        <h3 class="text-4xl lg:text-5xl font-bold">Hi, I'm Donatien !</h3>
+        <h4 class="text-3xl lg:text-4xl" id="work">Full-Stack Developper</h4>
+      </div>
+      <div class="text-xs italic sm:text-sm md:text-base w-1/3">  
+        <p>
+          I am a Full-Stack Developer. Currently starting my freelance business. 
+        </p>   
+        <p class="hidden xl:block">
+          If you need someone like me, don't hesitate to contact me because it'll be a pleasure to start working with you.
+        </p>
+      </div>
     </div>
-    <div class="text-xs italic sm:text-sm md:text-base w-1/3">  
-      <p>
-        I am a Full-Stack Developer. Currently starting my freelance business. 
-      </p>   
-      <p class="hidden xl:block">
-        If you need someone like me, don't hesitate because it'll be a pleasure to start working with you.
-      </p>
-    </div>
+    <img src="/logo.png" class="hidden md:block w-64 lg:w-96 xl:w-1/3 p-16 drop-shadow-xl">
   </section>
 </template>
 
