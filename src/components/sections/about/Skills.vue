cdc0daa8d08d70077db0cbb98a48f6a81043a0ae
rekky1aws <killikomfr@gmail.com>
2025-02-06 19:27:56 +0100

    Added Skills

--- src/components/sections/about/Skills.vue
+++ src/components/sections/about/Skills.vue
@@ -6,14 +6,54 @@
 	<article id="skills">
 		<h6>Skills</h6>
 		<div class="grid grid-rows-4 grid-cols-3 gap-4">
-			<a href=""><img src=""></a>
-			<a href=""><img src=""></a>
-			<a href=""><img src=""></a>
-			<a href=""><img src=""></a>
-			<a href=""><img src=""></a>
-			<a href=""><img src=""></a>
-			<a href=""><img src=""></a>
-			<a href=""><img src=""></a>
+			<a class="image-container hover-tilt" href="">
+				<img src="/skills/html5.png" alt="HTML5">
+				<span>HTML5</span>
+			</a>
+			<a class="image-container hover-tilt" href="">
+				<img src="/skills/css3.png" alt="CSS3">
+				<span>CSS3</span>
+			</a>
+			<a class="image-container hover-tilt" href="">
+				<img src="/skills/javascript.png" alt="JavaScript">
+				<span>JavaScript</span>
+			</a>
+			<a class="image-container hover-tilt" href="">
+				<img src="/skills/php.png" alt="PHP">
+				<span>PHP</span>
+			</a>
+			<a class="image-container hover-tilt" href="">
+				<img src="/skills/sass.png" alt="SASS">
+				<span>SASS</span>
+			</a>
+			<a class="image-container hover-tilt" href="">
+				<img src="/skills/ts.png" alt="TypeScript">
+				<span>TypeScript</span>
+			</a>
+			<a class="image-container hover-tilt" href="">
+				<img src="/skills/symfony.png" alt="Symfony">
+				<span>Symfony</span>
+			</a>
+			<a class="image-container hover-tilt" href="">
+				<img src="/skills/tailwind.png" alt="TailwindCSS">
+				<span>TailwindCSS</span>
+			</a>
+			<a class="image-container hover-tilt" href="">
+				<img src="/skills/vuejs.png" alt="VueJS">
+				<span>VueJS</span>
+			</a>
+			<a class="image-container hover-tilt" href="">
+				<img src="/skills/bash.png" alt="BASH">
+				<span>BASH</span>
+			</a>
+			<a class="image-container hover-tilt" href="">
+				<img src="/skills/apache.png" alt="Apache">
+				<span>Apache</span>
+			</a>
+			<a class="image-container hover-tilt" href="">
+				<img src="/skills/c.png" alt="C">
+				<span>C</span>
+			</a>
 		</div>
 	</article>
 </template>
