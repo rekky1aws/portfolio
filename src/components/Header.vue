053786289ae7cb7ee1f34913b7ae769735047553
rekky1aws <killikomfr@gmail.com>
2025-02-10 14:08:53 +0100

    Fixed elements going over the header on scroll.
    Added a z-index to the header.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <script type="text/javascript"></script>
 
 <template>
-	<header class="bg-gray-200 drop-shadow p-5 flex flex-reverse items-center justify-between sticky top-0">
+	<header class="bg-gray-200 drop-shadow p-5 flex flex-reverse items-center justify-between sticky top-0 z-10">
 		<div class="flex gap-5">
 			<img src="/pp.jpg" class="size-16 rounded-full drop-shadow">
 			<div>
@@ -10,9 +10,9 @@
 			</div>
 		</div>
 
-
 		<nav>
-				
+			<!-- List Section -->
+			<!-- Burger on smaller devices -->
 		</nav>
 	</header>
 </template>
