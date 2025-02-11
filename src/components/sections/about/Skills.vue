a263c24790617a74b46f1fd3607a3d3015898525
rekky1aws <killikomfr@gmail.com>
2025-02-11 17:30:11 +0100

    Better skills display
    Changed the grid of skills for 2xl display (from a 3x4 grid to a 4x3
    grid)

--- src/components/sections/about/Skills.vue
+++ src/components/sections/about/Skills.vue
@@ -5,52 +5,52 @@
 <template>
 	<article id="skills">
 		<h6>Skills</h6>
-		<div class="grid grid-rows-4 grid-cols-3 gap-4">
-			<a class="image-container hover-tilt" href="">
+		<div class="grid grid-rows-4 grid-cols-3 gap-4 2xl:grid-rows-3 2xl:grid-cols-4">
+			<a class="image-container hover-tilt 2xl:order-1" href="">
 				<img src="/skills/html5.png" alt="HTML5">
 				<span>HTML5</span>
 			</a>
-			<a class="image-container hover-tilt" href="">
+			<a class="image-container hover-tilt 2xl:order-5" href="">
 				<img src="/skills/css3.png" alt="CSS3">
 				<span>CSS3</span>
 			</a>
-			<a class="image-container hover-tilt" href="">
+			<a class="image-container hover-tilt 2xl:order-9" href="">
 				<img src="/skills/javascript.png" alt="JavaScript">
 				<span>JavaScript</span>
 			</a>
-			<a class="image-container hover-tilt" href="">
+			<a class="image-container hover-tilt 2xl:order-2" href="">
 				<img src="/skills/php.png" alt="PHP">
 				<span>PHP</span>
 			</a>
-			<a class="image-container hover-tilt" href="">
+			<a class="image-container hover-tilt 2xl:order-6" href="">
 				<img src="/skills/sass.png" alt="SASS">
 				<span>SASS</span>
 			</a>
-			<a class="image-container hover-tilt" href="">
+			<a class="image-container hover-tilt 2xl:order-10" href="">
 				<img src="/skills/ts.png" alt="TypeScript">
 				<span>TypeScript</span>
 			</a>
-			<a class="image-container hover-tilt" href="">
+			<a class="image-container hover-tilt 2xl:order-3" href="">
 				<img src="/skills/symfony.png" alt="Symfony">
 				<span>Symfony</span>
 			</a>
-			<a class="image-container hover-tilt" href="">
+			<a class="image-container hover-tilt 2xl:order-7" href="">
 				<img src="/skills/tailwind.png" alt="TailwindCSS">
 				<span>TailwindCSS</span>
 			</a>
-			<a class="image-container hover-tilt" href="">
+			<a class="image-container hover-tilt 2xl:order-11" href="">
 				<img src="/skills/vuejs.png" alt="VueJS">
 				<span>VueJS</span>
 			</a>
-			<a class="image-container hover-tilt" href="">
+			<a class="image-container hover-tilt 2xl:order-4" href="">
 				<img src="/skills/apache.png" alt="Apache">
 				<span>Apache</span>
 			</a>
-			<a class="image-container hover-tilt" href="">
+			<a class="image-container hover-tilt 2xl:order-8" href="">
 				<img src="/skills/bash.png" alt="BASH">
 				<span>BASH</span>
 			</a>
-			<a class="image-container hover-tilt" href="">
+			<a class="image-container hover-tilt 2xl:order-12" href="">
 				<img src="/skills/nodejs.png" alt="NodeJS">
 				<span>NodeJS</span>
 			</a>
