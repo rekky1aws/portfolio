1b84654463d1e5edd20db038b6b10727d79e67a0
rekky1aws <killikomfr@gmail.com>
2025-02-15 20:19:19 +0100

    Contact.vue address

--- src/components/sections/Contact.vue
+++ src/components/sections/Contact.vue
@@ -13,6 +13,11 @@
 			<div>E-Mail :</div>
 			<span>donatienhochart2@gmail.com</span>
 		</article>
+		<article>
+			<div>Address :</div>
+			<span>39 rue Marot,</span>
+			<span>FR 62160, Bully-les-Mines</span>
+		</article>
 	</section>
 </template>
 
