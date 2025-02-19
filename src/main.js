4b4c76f5f9f09ec2a067dabf31c9c50a69ae51f3
rekky1aws <killikomfr@gmail.com>
2025-02-19 20:19:19 +0100

    Importing AnimateOnScroll from PrimeVue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
 import App from './App.vue';
+
+import AnimateOnScroll from 'primevue/animateonscroll';
 import './style.css';
 
 const app = createApp(App).mount('#app');
 app.use(PrimeVue);
+app.directive('animateonscroll', AnimateOnScroll);
