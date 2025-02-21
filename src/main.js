eaf67f5ebfe25cef004193f0e241831704566c20
rekky1aws <killikomfr@gmail.com>
2025-02-21 20:19:19 +0100

    Roll-and-tilt directive
    Added a `roll-and-tilt` directive to v-motion.
    Rolls and fades the concerned elements in when they appear.
    Roll out and fade out when elements leave viewport.
    Hovered Elements are made slightly more opaque, big, and tilt a bit to
    the right.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
 import { createApp } from 'vue';
-import PrimeVue from 'primevue/config';
 import App from './App.vue';
+import { MotionPlugin } from '@vueuse/motion';
 
-import AnimateOnScroll from 'primevue/animateonscroll';
 import './style.css';
 
-const app = createApp(App).mount('#app');
-app.use(PrimeVue);
-app.directive('animateonscroll', AnimateOnScroll);
+const app = createApp(App);
+
+app.use(MotionPlugin, {
+	directives: {
+		'roll-and-tilt': {
+			initial: {
+				opacity: 0,
+				rotate: '90deg',
+			},
+			visible: {
+				opacity: 0.8,
+				rotate: '0deg',
+			},
+			hovered: {
+				opacity: 1,
+				scale: 1.05,
+				rotate: '5deg',
+			},
+			leave: {
+				opacity: 0,
+				rotate: '90deg',
+			}
+		},
+	}
+});
+app.mount('#app');
