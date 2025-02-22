a1a15c795cf5bbb9fe5de815b03e5a2dd5c14046
rekky1aws <killikomfr@gmail.com>
2025-02-22 20:19:19 +0100

    Applied roll-and-tilt directive to skills

--- src/components/sections/about/Skills.vue
+++ src/components/sections/about/Skills.vue
@@ -6,51 +6,51 @@
 	<article id="skills">
 		<h6>Skills</h6>
 		<div class="grid grid-rows-4 grid-cols-3 gap-4 2xl:grid-rows-3 2xl:grid-cols-4 2xl:max-h-[50vh] 2xl:gap-8">
-			<a class="image-container hover-tilt 2xl:order-1" href="">
+			<a class="image-container 2xl:order-1" href="" v-motion-roll-and-tilt >
 				<img src="/skills/html5.png" alt="HTML5">
 				<span>HTML5</span>
 			</a>
-			<a class="image-container hover-tilt 2xl:order-5" href="">
+			<a class="image-container 2xl:order-5" href="" v-motion-roll-and-tilt>
 				<img src="/skills/css3.png" alt="CSS3">
 				<span>CSS3</span>
 			</a>
-			<a class="image-container hover-tilt 2xl:order-9" href="">
+			<a class="image-container 2xl:order-9" href="" v-motion-roll-and-tilt>
 				<img src="/skills/javascript.png" alt="JavaScript">
 				<span>JavaScript</span>
 			</a>
-			<a class="image-container hover-tilt 2xl:order-2" href="">
+			<a class="image-container 2xl:order-2" href="" v-motion-roll-and-tilt>
 				<img src="/skills/php.png" alt="PHP">
 				<span>PHP</span>
 			</a>
-			<a class="image-container hover-tilt 2xl:order-6" href="">
+			<a class="image-container 2xl:order-6" href="" v-motion-roll-and-tilt>
 				<img src="/skills/sass.png" alt="SASS">
 				<span>SASS</span>
 			</a>
-			<a class="image-container hover-tilt 2xl:order-10" href="">
+			<a class="image-container 2xl:order-10" href="" v-motion-roll-and-tilt>
 				<img src="/skills/ts.png" alt="TypeScript">
 				<span>TypeScript</span>
 			</a>
-			<a class="image-container hover-tilt 2xl:order-3" href="">
+			<a class="image-container 2xl:order-3" href="" v-motion-roll-and-tilt>
 				<img src="/skills/symfony.png" alt="Symfony">
 				<span>Symfony</span>
 			</a>
-			<a class="image-container hover-tilt 2xl:order-7" href="">
+			<a class="image-container 2xl:order-7" href="" v-motion-roll-and-tilt>
 				<img src="/skills/tailwind.png" alt="TailwindCSS">
 				<span>TailwindCSS</span>
 			</a>
-			<a class="image-container hover-tilt 2xl:order-11" href="">
+			<a class="image-container 2xl:order-11" href="" v-motion-roll-and-tilt>
 				<img src="/skills/vuejs.png" alt="VueJS">
 				<span>VueJS</span>
 			</a>
-			<a class="image-container hover-tilt 2xl:order-4" href="">
+			<a class="image-container 2xl:order-4" href="" v-motion-roll-and-tilt>
 				<img src="/skills/apache.png" alt="Apache">
 				<span>Apache</span>
 			</a>
-			<a class="image-container hover-tilt 2xl:order-8" href="">
+			<a class="image-container 2xl:order-8" href="" v-motion-roll-and-tilt>
 				<img src="/skills/bash.png" alt="BASH">
 				<span>BASH</span>
 			</a>
-			<a class="image-container hover-tilt 2xl:order-12" href="">
+			<a class="image-container 2xl:order-12" href="" v-motion-roll-and-tilt>
 				<img src="/skills/nodejs.png" alt="NodeJS">
 				<span>NodeJS</span>
 			</a>
