fe156b268f14d04abd6da4e87f1c00767d926ca5
rekky1aws <killikomfr@gmail.com>
2025-02-27 20:19:19 +0100

    Project component init

--- src/components/sections/Projects.vue
+++ src/components/sections/Projects.vue
@@ -1,9 +1,13 @@
 <script setup>
+	import ProjectContainer from './projects/ProjectContainer.vue'
 </script>
 
 <template>
 	<section id="projects">
 		<h5>My Work</h5>
+		<div class="grid gird-cols-3">
+			<ProjectContainer name="Triple Drain" imgSrc=""></ProjectContainer>
+		</div>
 	</section>
 </template>
 
