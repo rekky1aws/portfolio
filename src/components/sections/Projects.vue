b85ce41e39b01055537976f62222a6cba18e7115
rekky1aws <killikomfr@gmail.com>
2025-03-04 20:19:19 +0100

    Projects and ProjectContainer
    Moved ProjectContainer to a more logical folder.
    Triple Drain as the first project.

--- src/components/sections/Projects.vue
+++ src/components/sections/Projects.vue
@@ -1,12 +1,14 @@
 <script setup>
-	import ProjectContainer from './projects/ProjectContainer.vue'
+	import ProjectContainer from '../subcomponents/ProjectContainer.vue'
 </script>
 
 <template>
 	<section id="projects">
 		<h5>My Work</h5>
-		<div class="grid gird-cols-3">
-			<ProjectContainer name="Triple Drain" imgSrc=""></ProjectContainer>
+		<div class="grid grid-cols-3">
+			<ProjectContainer name="Triple Drain" imgSrc="" link="">
+				<template #description>Triple Drain is a webapp to easily see scores from the main Zen Studios Pinball Games (Pinball FX and Pinball M) and process global rankings.</template>
+			</ProjectContainer>
 		</div>
 	</section>
 </template>
