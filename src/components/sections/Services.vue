823978421c73d78953b1de63247837859f553626
rekky1aws <killikomfr@gmail.com>
2025-03-05 16:00:57 +0100

    ServiceContainer as subcomponents
    DRYed the code with a subcomponent for the services that is similar to
    the ProjectContainer but different to fit the needs for a service
    better.

--- src/components/sections/Services.vue
+++ src/components/sections/Services.vue
@@ -1,40 +1,43 @@
 <script setup>
+import ServiceContainer from '../subcomponents/ServiceContainer.vue'
 </script>
 
 <template>
 	<section id="services">
 		<h5>Services</h5>
 		<div class="grid gap-6 grid-cols-3 items-start">
-			<article class="flex flex-col gap-2">
-				<h6 class="text-center">Online Storefront</h6>
-				<img src="">
-				<p class="text-justify">
-					Nowadays it's very important to have an online presence. Generally the minimum required is a simple website with all the information about your business like the location, the schedule or the products you have.
-				</p>
-				<p class="text-justify">
-					If you simply want to keep your clients updated and spread informations for future clients about your business, this is the best option. 
-				</p>
-			</article>
-			<article class="flex flex-col justify-center gap-2">
-				<h6 class="text-center">Web App with Simple Backend</h6>
-				<img src="">
-				<p class="text-justify">
-					When your business is a bit more developped, you might need a more powerful website. One that can do price estimation, or managing your stocks. It can be really helpful and make you gain a lot of time to take care of other things.
-				</p>
-				<p class="text-justify">
-					Speed things up with your business by opting for this option.
-				</p>
-			</article>
-			<article class="flex flex-col justify-center gap-2">
-				<h6 class="text-center">Complete Management App</h6>
-				<img src="">
-				<p class="text-justify">
-					As your business continues to grow, you'll need to automatize more and more things. At this point, a complete web application to manage stocks, clients and their orders, invoices and payments.
-				</p>
-				<p class="text-justify">
-					Then this is the option you need : a complete backoffice linked to the client side of your website. And if you need even more !
-				</p>
-			</article>
+			<ServiceContainer name="Online Storefront" imgSrc="" link="">
+				<template #description>
+					<p>
+						Nowadays it's very important to have an online presence. Generally the minimum required is a simple website with all the information about your business like the location, the schedule or the products you have.
+					</p>
+					<p>
+						If you simply want to keep your clients updated and spread informations for future clients about your business, this is the best option. 
+					</p>
+				</template>
+			</ServiceContainer>
+
+			<ServiceContainer name="Web App with Simple Backend" imgSrc="" link="">
+				<template #description>
+					<p>
+						When your business is a bit more developped, you might need a more powerful website. One that can do price estimation, or managing your stocks. It can be really helpful and make you gain a lot of time to take care of other things.
+					</p>
+					<p>
+						Speed things up with your business by opting for this option.
+					</p>
+				</template>
+			</ServiceContainer>
+
+			<ServiceContainer name="Complete Management App" imgSrc="" link="">
+				<template #description>
+					<p>
+						As your business continues to grow, you'll need to automatize more and more things. At this point, a complete web application to manage stocks, clients and their orders, invoices and payments.
+					</p>
+					<p>
+						Then this is the option you need : a complete backoffice linked to the client side of your website. And if you need even more !
+					</p>
+				</template>
+			</ServiceContainer>
 		</div>
 	</section>
 </template>
---
+++ src/components/subcomponents/ServiceContainer.vue
@@ -0,0 +1,26 @@
+<script setup>
+import OpenLink from '../icons/OpenLink.vue'
+
+defineProps({
+	name: String,
+	imgSrc: String,
+	link: String
+})
+</script>
+
+<template>
+	<div class="flex flex-col items-center gap-2">
+		<h6 class="font-bold text-xl text-center">{{ name }}</h6>
+		<img :src="imgSrc">
+		<div class="text-justify flex flex-col gap-2">
+			<slot name="description"/>
+		</div>
+		<div class="flex justify-end w-full">
+			<a class="text-green-800 font-bold hover-tilt flex gap-1 items-center justify-center m-2" target="_blank" :href="link">View <OpenLink height="15px" color="#166534"/></a>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+	
+</style> 
