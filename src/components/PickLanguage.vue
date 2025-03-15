1c451761fe6395b34c403c4c1ba8cf0808793a9c
rekky1aws <killikomfr@gmail.com>
2025-03-15 20:19:19 +0100

    Language Picker

---
+++ src/components/PickLanguage.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+
+const welcomeText = ref("Welcome");
+console.log(welcomeText);
+</script>
+
+<template>
+    <main class="h-screen w-screen flex flex-col justify-center items-center gap-8">
+        <h1 id="welcome" class="text-5xl font-bold">{{ welcomeText }}</h1>
+        <div class="flex justify-center items-center gap-8">
+            <RouterLink to="/fr" id="fr" @mouseover="welcomeText = 'Bienvenue'">
+                <div class="p-4 bg-gray-300 rounded-full drop-shadow hover-tilt flex gap-2 flex-col justify-center items-center text-4xl hover:drop-shadow-xl">
+                    <span>🇫🇷</span>
+                    <!-- <span>Français</span> -->
+                </div>
+            </RouterLink>
+            <RouterLink to="/en" id="en" @mouseover="welcomeText = 'Welcome'">
+                <div class="p-4 bg-gray-300 rounded-full drop-shadow hover-tilt flex gap-2 flex-col justify-center items-center text-4xl hover:drop-shadow-xl">
+                    <span>🇬🇧</span>
+                    <!-- <span>English</span> -->
+                </div>
+            </RouterLink>
+        </div>
+    </main>
+</template>
+
+<style scoped>
+</style>
