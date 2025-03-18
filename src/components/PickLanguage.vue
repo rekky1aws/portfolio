9d8ba67a00407d3a300d7d0513a61635b9e836f6
rekky1aws <killikomfr@gmail.com>
2025-03-18 20:18:08 +0100

    Better language picker.
    Changed the way the titles are dynamically displayed when hovering a
    flag.

--- src/components/PickLanguage.vue
+++ src/components/PickLanguage.vue
@@ -1,24 +1,27 @@
 <script setup>
 import { ref } from 'vue';
 
-const welcomeText = ref("Welcome");
-console.log(welcomeText);
+const language = ref("en");
 </script>
 
 <template>
-    <main class="h-screen w-screen flex flex-col justify-center items-center gap-8">
-        <h1 id="welcome" class="text-5xl font-bold">{{ welcomeText }}</h1>
-        <div class="flex justify-center items-center gap-8">
-            <RouterLink to="/fr" id="fr" @mouseover="welcomeText = 'Bienvenue'">
-                <div class="p-4 bg-gray-300 rounded-full drop-shadow hover-tilt flex gap-2 flex-col justify-center items-center text-4xl hover:drop-shadow-xl">
-                    <span>🇫🇷</span>
-                    <!-- <span>Français</span> -->
-                </div>
-            </RouterLink>
-            <RouterLink to="/en" id="en" @mouseover="welcomeText = 'Welcome'">
+    <main class="h-screen w-screen flex flex-col justify-center items-center">
+        <h1 v-if="language == 'fr'" class="text-5xl font-bold">Bienvenue</h1>
+        <h1 v-else class="text-5xl font-bold">Welcome</h1>
+
+        <h2 v-if="language == 'fr'" class="font-bold m-2">Choisissez une langue</h2>
+        <h2 v-else class="font-bold m-2">Pick a language</h2>
+
+
+        <div class="flex justify-center items-center gap-8 m-6">
+            <RouterLink to="/en" id="en" @mouseover="language = 'en'">
                 <div class="p-4 bg-gray-300 rounded-full drop-shadow hover-tilt flex gap-2 flex-col justify-center items-center text-4xl hover:drop-shadow-xl">
                     <span>🇬🇧</span>
-                    <!-- <span>English</span> -->
+                </div>
+            </RouterLink>
+            <RouterLink to="/fr" id="fr" @mouseover="language = 'fr'">
+                <div class="p-4 bg-gray-300 rounded-full drop-shadow hover-tilt flex gap-2 flex-col justify-center items-center text-4xl hover:drop-shadow-xl">
+                    <span>🇫🇷</span>
                 </div>
             </RouterLink>
         </div>
