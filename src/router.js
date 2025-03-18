ed12e78ccf1e1eacb5b4ce9c72c4ce5da75d573a
rekky1aws <killikomfr@gmail.com>
2025-03-18 20:29:34 +0100

    Trying to automate language picking
    Tried to make `router.js` choose language automatically depending on
    user's prefered language.
    It didn't really work so I commented it but I think I might be on the
    right path, I'll just need to look through it a bit more.

--- src/router.js
+++ src/router.js
@@ -5,20 +5,36 @@ import English from './components/English.vue';
 import PickLanguage from './components/PickLanguage.vue';
 
 const routes = [
-    { path: '/', component: PickLanguage },
+  { path: '/', component: PickLanguage },
 	{ path: '/fr', component: French },
 	{ path: '/en', component: English },
   ];
   
 const router = createRouter({
-history: createMemoryHistory(),
-routes,
+  history: createMemoryHistory(),
+  routes,
 });
 
 const routeLanguage = window.location.href.split('/').pop();
 
-const userLanguage = navigator.language;
+const userLanguage = navigator.language.split('-')[0];
 
-console.log(routeLanguage);
+// Choosing language automatically depending on user's prefered language
+// switch (userLanguage) {
+//   case 'en':
+//     router.replace('/en');
+//     break;
+
+//   case 'fr':
+//     router.replace('/fr');
+//     break;
+
+//   default:
+//     router.replace('/');
+// }
+
+// DEBUG
+// console.log("routeLanguage : ", routeLanguage);
+// console.log("userLanguage : ", userLanguage);
 
 export default router;
