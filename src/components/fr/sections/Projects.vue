5196785486c96c26e548321d58441086fa01e17d
rekky1aws <killikomfr@gmail.com>
2025-03-29 20:19:19 +0100

    FR Wii Menu Recreation added to projects.

--- src/components/fr/sections/Projects.vue
+++ src/components/fr/sections/Projects.vue
@@ -5,10 +5,16 @@
 <template>
 	<section id="projects">
 		<h5>Mes projets</h5>
-		<div class="grid grid-cols-3">
+		<div class="grid grid-cols-3 gap-6">
 			<ProjectContainer name="Triple Drain" imgSrc="" link="">
 				<template #description>
-					Triple Drain est une application web regroupant les scores sur les jeux principaux de Flipper de Zen Studios (Pinball FX et Pinball M) afin de les consulter plus facilement et calculer un classement global.
+					Triple Drain est une application web regroupant les scores sur les jeux principaux de Flipper de Zen Studios
+					(Pinball FX et Pinball M) afin de les consulter plus facilement et calculer un classement global.
+				</template>
+			</ProjectContainer>
+			<ProjectContainer name="Wii Menu Recreation" imgSrc="" link="">
+				<template #description>
+					C'est l'endroit ou je m'amuse à créer des petits jeux / petites expériences en HTML, CSS et JS uniquement (sans frameworks). Dès que j'ai une idée et un peu de temps, j'y rajoute quelque chose.
 				</template>
 			</ProjectContainer>
 		</div>
